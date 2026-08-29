c330007069b89f3834fb632da72b5bc2703a7605
Yoosuf-Eenas-Jaufar <yoosuf.yj@gmail.com>
2026-08-29 19:06:10 +0500

    chore: migrate starter project to Expo SDK 54

---
+++ app.config.js
@@ -0,0 +1,37 @@
+export default {
+  expo: {
+    name: 'Dat Expense',
+    slug: 'dat-expense',
+    version: '1.0.0',
+    orientation: 'portrait',
+
+    icon: './assets/icon.png',
+
+    userInterfaceStyle: 'light',
+
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+
+    assetBundlePatterns: ['**/*'],
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+
+    platforms: ['ios', 'android'],
+
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
